061bf4d4df09469fb90cbdaee86dc2f83a4ba437
Your Name <you@example.com>
2026-05-06 17:56:06 +0500

    debug3

--- routes/auth.js
+++ routes/auth.js
@@ -1,67 +1,63 @@
-import express from "express"
-import User from "../models/user.js"
-import bcrypt from "bcryptjs"
-import JWT from "jsonwebtoken"
+import express from "express";
+import User from "../models/user.js";
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
 
-let route = express.Router()
-
+let route = express.Router();
 
 route.post("/register", async (req, res) => {
-   try{
-     let {username, email, password, role} = req.body
+  try {
+    let { username, email, password, role } = req.body;
 
-    let eUser = await User.findOne({email})
+    let eUser = await User.findOne({ email });
 
     // validation
 
-    if(eUser){
-       return res.status(400).send({ message: "This user already exist" });
+    if (eUser) {
+      return res.status(400).send({ message: "This user already exist" });
     }
-    const salt = await bcrypt.genSalt(10)
+    const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const user = await User.create({
-        username,
-        email,
-        password: hashedPassword,
-        role
-    })
+      username,
+      email,
+      password: hashedPassword,
+      role,
+    });
 
-   return res.send({message: "User created", userId: user._id})
-   }catch(err) {
-   return res.send({ message: err.message });
-   }
-
-})
+    return res.send({ message: "User created", userId: user._id });
+  } catch (err) {
+    return res.send({ message: err.message });
+  }
+});
 
 route.post("/login", async (req, res) => {
-    let {email, password} = req.body
+  let { email, password } = req.body;
 
-    let user = await User.findOne({email})
+  let user = await User.findOne({ email });
 
-    
-
-    if(!user) return res
+  if (!user)
+    return res
       .status(400)
       .send({ message: "This email has not been already registered" });
 
-        let isMatch = await bcrypt.compare(password, user.password)
+  let isMatch = await bcrypt.compare(password, user.password);
 
-        if(!isMatch) return res.status(400).send({ message: "Password is incorrect" });
+  if (!isMatch)
+    return res.status(400).send({ message: "Password is incorrect" });
 
-        const token =  JWT.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
-          expiresIn: process.env.JWT_EXPIRE,
-        });
+  const token = JWT.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET,
+  );
 
-       return res.send({message: "Login success!", token})
-    
-})
+  return res.send({ message: "Login success!", token });
+});
 
 route.get("/users", async (req, res) => {
-    let users = await User.find()
-    res.send(users)
-})
+  let users = await User.find();
+  res.send(users);
+});
 
 export default route;
-
-
